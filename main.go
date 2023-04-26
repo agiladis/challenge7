cae805d873d5b78523beb9112c9e413a540cac77
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-04-26 12:35:56 +0700

    init go project, create post request using static payload

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type JsonPayload struct {
+	Water int `json:"water"`
+	Wind  int `json:"wind"`
+}
+
+func main() {
+	var jsonPayload = JsonPayload{
+		Water: 13,
+		Wind:  8,
+	}
+
+	jsonByte, err := json.Marshal(jsonPayload)
+	if err != nil {
+		panic(err)
+	}
+
+	var client http.Client
+
+	req, err := http.NewRequest("POST", "https://jsonplaceholder.typicode.com/posts", bytes.NewBuffer(jsonByte))
+	req.Header.Add("Content-Type", "application/json")
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+}
